6b6ab6354a53c92926dc486b8e5f80b4677ac1fa
chhayab54 <chhaya.10@gmail.com>
2023-06-06 20:41:10 +0530

    version control

--- variables.sh
+++ variables.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# How to print the variables
+
 a=10
 b=20
 c=30
 
 echo Value of A is $a
 echo Value of B is "$b"
-echo "Value of C is ${a}"
+echo "Value of C is \e[33m; ${a} \e[0m]"
+
