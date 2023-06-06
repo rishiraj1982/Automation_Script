1d4b1271fa153e84d658512b56f4a04cdf1b0c7d
chhayab54 <chhaya.10@gmail.com>
2023-06-06 20:18:21 +0530

    version control

--- Color.sh
+++ Color.sh
@@ -6,4 +6,4 @@
 echo -e "\e[32m I am printing in color \e[0m"
 echo -e "\e[33m I am printing in color \e[0m"
 
-echo -e "\e[43,32m I am printing in color \e[0m"
+echo -e "\e[43;32m I am printing in color \e[0m"
