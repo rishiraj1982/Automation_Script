32c3e93e667db14ef0c6d12ac15461ec18825dc1
chhayab54 <chhaya.10@gmail.com>
2023-06-06 20:15:08 +0530

    version control

--- Color.sh
+++ Color.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Print text in color format
+# Google and take the value of color as per Bash script
 
 echo -e "\e[32m I am printing in color \e[0n"
+echo -e "\e[33m I am printing in color \e[0n"
 
