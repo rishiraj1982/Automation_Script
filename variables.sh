0fe5916215270f0ee55e2158896a4f4be067722a
chhayab54 <chhaya.10@gmail.com>
2023-06-06 20:42:57 +0530

    version control

--- variables.sh
+++ variables.sh
@@ -8,7 +8,7 @@ c=30
 
 echo Value of A is $a
 echo Value of B is "$b"
-echo -e "Value of C is \e[33m; ${a} \e[0m]    # value will be print in color #
+echo -e "Value of C is \e[33m; ${a} \e[0m"   # value will be print in color #
 
 
 
